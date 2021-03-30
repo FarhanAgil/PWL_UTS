129cd68df92665118de993549c617b1c2c62d07e
FarhanAgil <drakek1977@gmail.com>
2021-03-30 15:53:52 +0700

    mengisi model barang.php

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Barang as Authenticable;
+use illuminate\Notification\Notifiable;
+use illuminate\Database\Eloquent\Model;
+
 
 class Barang extends Model
 {
     use HasFactory;
+    protected $table = "barangs";
+    public $timestamps = false;
+    protected $primaryKey = 'id_barang';
+
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty'
+    ];
+        
+    
 }
