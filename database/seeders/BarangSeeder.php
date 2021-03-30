adb006f35a342515fc1b224a2888138aaa7bcdfc
FarhanAgil <drakek1977@gmail.com>
2021-03-30 17:09:27 +0700

    modifikasi barang seeder dan database seeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -16,18 +16,31 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('barangs')->insert([
-            'kode_barang'=>['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005', 'PRD006', 'PRD007', 'PRD008', 'PRD009', 'PRD010',
-                            'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015', 'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'],
-            'nama_barang'=>['Indomie', 'Pocari Sweet', 'Silverqueen','lolipop','mie sedap','kopikap','bengbeng',
+            $kode_barang =['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005',
+                            'PRD006', 'PRD007', 'PRD008', 'PRD009', 'PRD010',
+                            'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015',
+                            'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'];
+            $nama_barang =['Indomie', 'Pocari Sweet', 'Silverqueen','lolipop','mie sedap','kopikap','bengbeng',
                             'Chocolatos','popmie','alpenliebe','whitecoffee','aleale','sarden','komo','madurasa',
-                            'citato','Jetz','sonice','fruitea','mrbread'],
-            'kategori_barang'=>['Makanan', 'Minuman', 'Snack', 'Permen','Makanan','minuman','Snack','Snack','Makanan','Permen',
-                                'Minuman','Minuman','Makanan','Snack','Minuman','Snack','Snack','Snack','minuman','Makanan'],
-            'harga'=>['3000', '6000', '12500','2000','3000','1000','1500','3000','7000','500','1500','1000','15000','500',
-                      '2000', '2000','1500','2500','7000','10000'],
-            'qty'=>['100', '50', '45','20','50','200','50','50','100','5000','1000','700','500','5000','700','500','200',
-                    '40','50','200']
-        ]);
-    }
+                            'citato','Jetz','sonice','fruitea','mrbread'];
+            $kategori_barang =['Makanan', 'Minuman', 'Snack', 'Permen','Makanan','Minuman','Snack','Snack','Makanan','Permen',
+                                'Minuman','Minuman','Makanan','Snack','Minuman','Snack','Snack','Snack','minuman','Makanan'];
+            $harga =['3000', '6000', '12500','2000','3000','1000','1500','3000','7000','500','1500','1000','15000','500',
+                    '2000', '2000','1500','2500','7000','10000'];
+            $qty =['100', '50', '45','20','50','200','50','50','100','5000','1000','700','500','5000','700','500','200',
+                    '40','50','200'];
+
+        for($i=0; $i < 20; $i++){
+            DB::table('barangs')->insert([
+                //'id_barang'=> $id_barang[$i],
+                'kode_barang' => $kode_barang[$i],
+                'nama_barang' => $nama_barang[$i],
+                'kategori_barang' => $kategori_barang[$i],
+                'harga' => $harga[$i],
+                'qty' => $qty[$i]
+            ]);
+            }
+        }
 }
+
+    
