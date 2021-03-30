658be8e246a52e50364f6a789a6471b75cb64d55
FarhanAgil <drakek1977@gmail.com>
2021-03-31 01:09:52 +0700

    memodifikasi model barang.php untuk inisialisasi pemanggilan tabel

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -3,26 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Barang as Authenticable;
-use illuminate\Notification\Notifiable;
-use illuminate\Database\Eloquent\Model;
-
+use Illuminate\Foundation\Auth\Barang as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
     use HasFactory;
     protected $table = "barangs";
-    public $timestamps = false;
     protected $primaryKey = 'id_barang';
 
     protected $fillable = [
-        'id_barang',
         'kode_barang',
         'nama_barang',
         'kategori_barang',
         'harga',
-        'qty'
+        'qty',
     ];
-        
-    
 }
